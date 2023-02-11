05c0ea6c2a804146eea55df13805b93eb9c0269d
kikoDevv <103362419+kikoDevv@users.noreply.github.com>
2023-02-12 00:02:04 +0100

    Initial Commit

---
+++ kiko/kikoApp.swift
@@ -0,0 +1,17 @@
+//
+//  kikoApp.swift
+//  kiko
+//
+//  Created by Kiko on 2023-02-12.
+//
+
+import SwiftUI
+
+@main
+struct kikoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
