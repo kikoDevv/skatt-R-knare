9470a30a29082af5ca3dc61e132f2001b998b681
kikoDevv <103362419+kikoDevv@users.noreply.github.com>
2023-02-12 00:27:09 +0100

    main screen with no stuff in it

--- kiko/ContentView.swift
+++ kiko/ContentView.swift
@@ -9,16 +9,60 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        NavigationView{
+            ZStack{
+                Circle()
+                    .scale(3)
+                    .foregroundColor(Color.blue)
+                Circle()
+                    .scale(1.7)
+                    .foregroundColor(Color.white.opacity(0.5))
+                    .offset(y:-34)
+                Circle()
+                    .scale(1.3)
+                    .offset(y:-34)
+                    .foregroundColor(Color.white)
+                 RoundedRectangle(cornerRadius: 20)
+                        .frame(width: 330, height:250)
+                        .offset(y:-34)
+                        .foregroundColor(Color.white)
+                        .shadow(radius: 5)
+            }
         }
-        .padding()
     }
 }
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//=============================================
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
